07b3f72dc627fd9bdac8b9094af80e8da5afc0ec
needarling1 <97009120+needarling1@users.noreply.github.com>
2025-04-21 17:57:01 -0700

    changed name

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -12,8 +12,8 @@ export default function App() {
     <NavigationContainer>
       <TabNav.Navigator>
         <TabNav.Screen name = "Home" component={HomeScreen} options={{ headerShown: false }}/>
-        <TabNav.Screen name = "ToDo" component={ToDoScreen} options={{ headerShown: false }}/>
-        <TabNav.Screen name = "MajorTracker" component={MajorTrackerScreen} options={{ headerShown: false }}/>
+        <TabNav.Screen name = "To Do" component={ToDoScreen} options={{ headerShown: false }}/>
+        <TabNav.Screen name = "Major Tracker" component={MajorTrackerScreen} options={{ headerShown: false }}/>
         <TabNav.Screen name = "Profile" component={ProfileScreen} options={{ headerShown: false }}/>
       </TabNav.Navigator>
     </NavigationContainer>
